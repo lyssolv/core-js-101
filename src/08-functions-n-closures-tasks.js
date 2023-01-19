850a89ef62ed1048a492c82a5cc3675c2d0c2c2c
lyssolv <annieway.hakobyan@gmail.com>
2023-01-19 21:22:14 +0200

    feat: solution to functions

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -184,8 +184,11 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  return function () {
+    // eslint-disable-next-line no-plusplus, no-param-reassign
+    return startFrom++;
+  };
 }
 
 
